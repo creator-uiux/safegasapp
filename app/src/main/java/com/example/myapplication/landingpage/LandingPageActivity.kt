6a59a945b8a2d9c35aed75f0ba58b1781fdd9a9e
Edmar Angelo Generoso <92077-12208861@SERVER2.DOMAIN>
2025-09-20 15:24:47 +0800

    message

---
+++ app/src/main/java/com/example/myapplication/landingpage/LandingPageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.landingpage
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class LandingPageActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing_page)
+    }
+}
